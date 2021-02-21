ff5dab1997e5f29ce9574d56e134316ebfc20274
sirdon <inform2theamit@gmail.com>
2021-02-21 19:45:18 +0530

    select page added

---
+++ src/pages/car/[make]/[brand]/[id].tsx
@@ -0,0 +1,75 @@
+import { GetServerSideProps } from "next"
+import { CarModel } from "../../../../../api/Car";
+import { openDB } from "../../../../openDB";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import { Button } from "@material-ui/core";
+import { useRouter } from "next/dist/client/router";
+
+const useStyles = makeStyles((theme) => ({
+
+  paper: {
+    padding: theme.spacing(2),
+    margin: 'auto',
+
+  },
+  img: {
+    width: '100%'
+  }
+}));
+interface CarDetailsProps {
+  car: CarModel | null | undefined;
+}
+export default function CarDetails({ car }: CarDetailsProps) {
+  const classes = useStyles();
+  const router = useRouter()
+  if (!car) {
+    return <div >Sorry, car not found!</div>
+  }
+  return ( //make, model, year, kilometers, fuelType, price, photoUrl, details
+    <div>
+      <Paper className={classes.paper}>
+        <Grid container spacing={2}>
+          <Grid item xs={12} sm={6} md={5}>
+            <img className={classes.img} alt="complex" src={car.photoUrl} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={7} container>
+            <Grid item xs container direction="column" spacing={2}>
+              <Grid item xs>
+                <Typography gutterBottom variant="h5">
+                  {car.make + " " + car.model}
+                </Typography>
+                <Typography gutterBottom variant="h4">${car.price}</Typography>
+                <Typography variant="body2" gutterBottom>
+                  Year: {car.year}
+                </Typography>
+                <Typography variant="body2" gutterBottom>
+                  Fuel Type: {car.fuelType}
+                </Typography>
+                <Typography gutterBottom variant="body2" color="textSecondary">
+                  KMs: {car.kilometers}
+                </Typography>
+                <Typography gutterBottom variant="body2" color="textSecondary">
+                  Details: {car.details}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Button variant="contained" color="primary" onClick={() => router.back()}>back</Button>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Paper>
+    </div>
+  );
+}
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const id = ctx.params.id;
+  const db = await openDB()
+  const car = await db.get<CarModel | undefined>('SELECT * FROM Car where id = ?', id);
+  return { props: { car: car || null } }
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,159 @@
-export default function Home() {
-  return <h1>Hello Youtube</h1>;
+import { Grid, Paper, makeStyles, InputLabel, MenuItem, FormControl, Select, SelectProps, Button } from "@material-ui/core";
+import { Field, Form, Formik, useField, useFormikContext } from "formik";
+import { GetServerSideProps } from "next";
+import { getMakes, Make } from "../database/getMakes";
+import React from 'react';
+import router, { useRouter } from "next/router";
+import { getModels, Model } from "../database/getModels";
+import { getAsString } from "../getAsString";
+import useSWR from "swr";
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    margin: 'auto',
+    maxWidth: 500,
+    padding: theme.spacing(3)
+  }
+}));
+
+export interface HomeProps {
+  makes: Make[];
+  models: Model[];
+}
+
+const prices = [500, 1000, 5000, 15000, 25000, 50000, 250000];
+export default function Home({ makes, models }: HomeProps) {
+  const classes = useStyles();
+  const { query } = useRouter();
+  const initialValues = {
+    make: getAsString(query.make) || 'all',
+    model: getAsString(query.model) || 'all',
+    minPrice: getAsString(query.minPrice) || 'all',
+    maxPrice: getAsString(query.maxPrice) || 'all'
+  }
+  return <Formik initialValues={initialValues} onSubmit={(values) => {
+    router.push({
+      pathname: '/',
+      query : {...values, page:1}
+    }, undefined, { shallow:true})
+   }}>
+    {({ values }) => (
+      <Form>
+        <Paper elevation={5} className={classes.paper}>
+          <Grid container spacing={3}>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth variant="outlined" >
+                <InputLabel id="search-make">Make</InputLabel>
+                <Field name="make" as={Select}
+                  labelId="search-make"
+                  label="Make"
+                >
+                  <MenuItem value="all">
+                    <em>All Makes</em>
+                  </MenuItem>
+                  {makes.map(make => (
+                    <MenuItem key={make.make} value={make.make}>
+                      {`${make.make} (${make.count})`}
+                    </MenuItem>
+                  ))}
+                </Field>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+            <ModelSelect make={values.make} name="model" models={models} ></ModelSelect>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth variant="outlined" >
+                <InputLabel id="search-min-price">Min Price</InputLabel>
+                <Field name="minPrice" as={Select}
+                  labelId="search-min-price"
+                  label="Min Price"
+                >
+                  <MenuItem value="all">
+                    <em>No Min</em>
+                  </MenuItem>
+                  {prices.map(price => (
+                    <MenuItem key={price} value={price}>
+                      {price}
+                    </MenuItem>
+                  ))}
+                </Field>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <FormControl fullWidth variant="outlined" >
+                <InputLabel id="search-max-price">Max Price</InputLabel>
+                <Field name="maxPrice" as={Select}
+                  labelId="search-max-price"
+                  label="Max Price"
+                >
+                  <MenuItem value="all">
+                    <em>No Max</em>
+                  </MenuItem>
+                  {prices.map(price => (
+                    <MenuItem value={price}>
+                      {price}
+                    </MenuItem>
+                  ))}
+                </Field>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button type="submit" variant="contained" color="primary" fullWidth > Search</Button>
+            </Grid>
+          </Grid>
+        </Paper>
+      </Form>
+    )}
+  </Formik>
+}
+
+export interface ModelSelectProps extends SelectProps{
+  name: string;
+  models: Model[];
+  make:string;
+}
+
+export function ModelSelect({models, make, ...props}: ModelSelectProps){
+  const {setFieldValue} = useFormikContext()
+  const [field] = useField({
+    name: props.name
+  });
+
+  const {data} = useSWR<Model[]>('/api/models?make=' + make,{
+    onSuccess: (newValues) =>{
+      if(!newValues.map(a=>a.model).includes(field.value)){
+        setFieldValue('model', 'all')
+      }
+    }
+  });
+  const newModels = data || models;
+  return <FormControl fullWidth variant="outlined" >
+  <InputLabel id="search-model">Model</InputLabel>
+  <Select name="model" 
+    labelId="search-model"
+    label="model"
+    {...field}
+    {...props}
+  >
+    <MenuItem value="all">
+      <em>All Models</em>
+    </MenuItem>
+    {newModels.map(model => (
+      <MenuItem key={model.model} value={model.model}>
+        {`${model.model} (${model.count})`}
+      </MenuItem>
+    ))}
+  </Select>
+</FormControl>
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const make = getAsString(ctx.query.make);
+  const [makes, models] = await Promise.all([
+    getMakes(), getModels(make)
+  ])
+  // const makes = await getMakes();
+  // const models = await getModels(make);
+  return { props: { makes, models } };
 }
